b8ef55adb73a52a04c79a044310e4136c924f2a3
ElTuni <110362446+ElTuni@users.noreply.github.com>
2025-04-25 18:02:59 -0300

    now it uploads from google drive

--- index.js
+++ index.js
@@ -1,97 +1,131 @@
+
+let pdf_db = ""
+const apiKey = "AIzaSyBbOHNyv0W7cgNS0UEh3zLnfJHJXU5_7Dg";
+async function getFolder(folderId) {
+  // Buscar archivos que estén dentro de esa carpeta
+  const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${apiKey}&fields=files(id,name,mimeType,webViewLink,webContentLink)`
+  const respuesta = await fetch(url)
+  const respuesta_promise = await respuesta.json()
+  pdf_db = await respuesta_promise.files
+}
+
+const folderId_main = "1Le13O6M6tlMAfRUUbQnSMFaojPvup9rS";
+getFolder(folderId_main)
+
+
+
+async function getFolder01(current_dicipline) {
+
+  // filtrar de todas las dicipline, cual es la elegida y obtener un obj
+  const drive_current_dicipline_obj = pdf_db.filter(pdf_unit => pdf_unit.name === current_dicipline)[0]
+
+  // buscamos en el drive, segun el id de la capeta de la diciplina
+  const url01 = `https://www.googleapis.com/drive/v3/files?q='${drive_current_dicipline_obj.id}'+in+parents&key=${apiKey}&fields=files(id,name,mimeType,webViewLink,webContentLink)`
+  
+  // esperamos hasta que haga los request
+  const response01 = await fetch(url01)
+  const response01_json = await response01.json()
+
+
+  const pdf_html = response01_json.files.map(pdf => `
+    <div class="download-div">
+      <div>
+        <p class="download-title">${pdf.name}</p>
+        <p class="download-actualizacion">Ultima actualización: 99/99/9999</p>
+      </div>
+        <button class="download-btn" onclick="window.open('${pdf.webViewLink}')"><i class="fa-regular fa-file-pdf"></i>Descargar</button>
+    </div>`).join('')
+
+
+  mainEl.innerHTML = `
+  <div class="subheader-background" id="background">
+    <div class="subheader-txt">
+      <h1>${capitalize(current_dicipline)}</h1>
+    </div>
+  </div>
+  <h2 class="description">Se desarrollaron formularios específicos para los procedimientos más frecuentes realizadas en el servicio de ${current_dicipline} de la Clínica Pasteur. <br>Recuerde que deben ser impresos, completados con los datos correspondientes, firmados por el paciente y validados con firma y sello del profesional médico interviniente.</h2>
+  <div class="download-container">
+    ${pdf_html}
+  </div>`;
+
+  document.getElementById("background").style.backgroundImage = `url('images/${current_dicipline}.jpg')`
+}
+
 const mainEl = document.getElementById("main-container")
 const backgroundEl = document.getElementById("background")
 
 const especialidades = [
     {
       nombre: "Anestesiología",
-      alt: "Icono de anestesiólogo con mascarilla",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"],
-      
+      alt: "Icono de anestesiólogo con mascarilla"
     },
     {
       nombre: "Cabeza y cuello",
-      alt: "Icono de cuello con zona tiroidea destacada",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"],
+      alt: "Icono de cuello con zona tiroidea destacada"
     },
     {
       nombre: "Cirugía bariátrica",
-      alt: "Icono de estómago reducido quirúrgicamente",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de estómago reducido quirúrgicamente"
     },
     {
       nombre: "Cirugía cardiovascular",
-      alt: "Icono de corazón con instrumentos quirúrgicos",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de corazón con instrumentos quirúrgicos"
     },
     {
       nombre: "Cirugía general",
-      alt: "Icono de médico con estetoscopio",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de médico con estetoscopio"
     },
     {
       nombre: "Cirugía maxilofacial",
-      alt: "Icono de perfil facial con mandíbula resaltada",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de perfil facial con mandíbula resaltada"
     },
     {
       nombre: "Cirugía neonatal",
-      alt: "Icono de bebé envuelto en mantita",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de bebé envuelto en mantita"
     },
     {
       nombre: "Coloproctología",
-      alt: "Icono de mano con guante sosteniendo intestino",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de mano con guante sosteniendo intestino"
     },
     {
       nombre: "Endoscopía",
-      alt: "Icono de tubo flexible entrando en sistema digestivo",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de tubo flexible entrando en sistema digestivo"
     },
     {
       nombre: "Ginecología y Obstetricia",
-      alt: "Icono de útero en estilo anatómico",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de útero en estilo anatómico"
     },
     {
       nombre: "Hemodinamia",
-      alt: "Icono de sistema circulatorio con flujos",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de sistema circulatorio con flujos"
     },
     {
       nombre: "Nefrología",
-      alt: "Icono de riñones con detalles anatómicos",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de riñones con detalles anatómicos"
     },
     {
       nombre: "Oftalmología",
-      alt: "Icono de ojo con detalles de visión",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf", "pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf", "pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf", "pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf", "pdf/Carta de Agradecimiento Mamá japonesa.pdf"]
+      alt: "Icono de ojo con detalles de visión"
     },
     {
       nombre: "Oncología",
-      alt: "Icono de célula con diagnóstico médico",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de célula con diagnóstico médico"
     },
     {
       nombre: "ORL",
-      alt: "Icono de oído, nariz y garganta en esquema",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de oído, nariz y garganta en esquema"
     },
     {
       nombre: "Traumatología",
-      alt: "Icono de pierna en movimiento con rayos de dolor",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de pierna en movimiento con rayos de dolor"
     },
     {
       nombre: "Trasplante hepático y cirugía hepatobiliar",
-      alt: "Icono de hígado con vena porta",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de hígado con vena porta"
     },
     {
       nombre: "Urología",
-      alt: "Icono de vejiga con sistema urinario resaltado",
-      pdf: ["pdf/Carta de Agradecimiento Mamá japonesa.pdf", "pdf/Carta de Agradecimiento Tanaka.pdf"]
+      alt: "Icono de vejiga con sistema urinario resaltado"
     }
   ];
 
@@ -113,36 +147,10 @@ mainEl.innerHTML += `
 document.addEventListener("click", function(e){
     if (e.target.dataset.especialidad) {
         const current_especialidad = e.target.dataset.especialidad
-        console.log(current_especialidad)
-        const current_especialidad_obj = (especialidades.filter(especialidad => deleteSpaces(especialidad.nombre) === capitalize(current_especialidad))[0])
-        console.log(current_especialidad_obj)
-
-        // faltan los pdf reales
-        const pdf_html = current_especialidad_obj.pdf.map(pdf => `
-          <div class="download-div">
-            <div>
-              <p class="download-title">Hepatograma nivel 4</p>
-              <p class="download-actualizacion">Ultima actualización: 99/99/9999</p>
-            </div>
-              <button class="download-btn" onclick="window.open('pdf/Carta de Agradecimiento Mamá japonesa.pdf')"><i class="fa-regular fa-file-pdf"></i>Descargar</button>
-          </div>`).join('')
-
-        mainEl.innerHTML = `
-        <div class="subheader-background" id="background">
-          <div class="subheader-txt">
-            <h1>${capitalize(current_especialidad_obj.nombre)}</h1>
-          </div>
-        </div>
-        <h2 class="description">Se desarrollaron formularios específicos para los procedimientos más frecuentes realizadas en el servicio de ${current_especialidad} de la Clínica Pasteur. <br>Recuerde que deben ser impresos, completados con los datos correspondientes, firmados por el paciente y validados con firma y sello del profesional médico interviniente.</h2>
-        <div class="download-container">
-          ${pdf_html}
-        </div>`;
-
-        document.getElementById("background").style.backgroundImage = `url('images/${e.target.dataset.especialidad}.jpg')`
+        getFolder01(current_especialidad)
     }
 })
 
-// Funcion para poner mayuscula a la primera letra
 function capitalize(word) {
     return word[0].toUpperCase() + word.slice(1)
 }
